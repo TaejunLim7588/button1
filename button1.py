24908a4bc99cb99f43d8e52eef87cd141f73c766
Taejun Lim <zmxngh2@naver.com>
2024-09-23 11:24:14 +0900

    text changed

--- button1.py
+++ button1.py
@@ -1,9 +1,8 @@
 from tkinter import *
-
 window =Tk()
 
-b1 = Button(window, text="Yeon Ryu")
-b2 = Button(window, text="60221010")
+b1 = Button(window, text="임태준")
+b2 = Button(window, text="60172187")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
